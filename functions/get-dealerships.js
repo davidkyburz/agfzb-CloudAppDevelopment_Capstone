88dde363ae1f7cdc92d5adfffd76bd42815a79f3
davidkyburz <davidkyburz@users.noreply.github.com>
2021-04-17 06:13:00 +0200

    cloud functions

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -7,6 +7,25 @@
   * @return The output of this action, which must be a JSON object.
   *
   */
- function main(params) {
-	return { message: 'Hello World' };
+
+function main1(params) {
+  return {
+    "query": {"selector": {"st": params.state}}
+ };
+}
+
+
+function main2(params) {
+  return {
+    entries: params.docs.map(doc => { return {
+      id: doc.id,
+      city: doc.city,
+      state: doc.state,
+      st: doc.st,
+      address: doc.address,
+      zip: doc.zip,
+      lat: doc.lat,
+      long: doc.long,
+    }})
+  };
 }
